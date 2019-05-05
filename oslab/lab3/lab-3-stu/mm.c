81eb012ca9c17a49877eb99e5d30e494f4d249b5
YxGuo <gyx_20170818@mail.ustc.edu.cn>
2019-05-05 21:18:26 +0800

    asd

--- oslab/lab3/lab-3-stu/mm.c
+++ oslab/lab3/lab-3-stu/mm.c
@@ -163,7 +163,7 @@ int mm_init(void)
     PUT(heap_listp + (15*WSIZE), PACK(0, PU_U));     //lenth 0, previous not used
 
     heap_listp += (14*WSIZE);//heap start addr
-    if ((bp = extend_heap(2*(CHUNKSIZE+8)) == NULL)
+    if ((bp = extend_heap(2*(CHUNKSIZE+8))) == NULL)
         return -1;
     return 0;
 }// Done!
