7bcbb3999de4734d1cb360b8e2c3ba3bc35f3fc8
YxGuo <gyx_20170818@mail.ustc.edu.cn>
2019-05-22 16:54:20 +0800

    asd

--- oslab/lab4/lab4-code/simple_fat16.c
+++ oslab/lab4/lab4-code/simple_fat16.c
@@ -238,7 +238,8 @@ FAT16 *pre_init_fat16(void)
   }
   fat16_ins->Bpb.Signature_word = *((WORD *)(&buffer[0x1fe]));
 
-  fat16_ins->FirstRootDirSecNum = 
+  fat16_ins->FirstRootDirSecNum = fat16_ins->Bpb.BPB_RsvdSecCnt+(fat16_ins->Bpb.BPB_NumFATS)*(fat16_ins->Bpb.BPB_FATSz16);
+  fat16_ins->FirstDataSector = fat16_ins->FirstRootDirSecNum+fat16_ins->Bpb.BPB_RootEntCnt;
   return fat16_ins;
 }
 
