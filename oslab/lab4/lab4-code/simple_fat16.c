3508c17fffba7c21a81c80c9f0c9d688b2714636
YxGuo <gyx_20170818@mail.ustc.edu.cn>
2019-05-22 20:27:18 +0800

    asds

--- oslab/lab4/lab4-code/simple_fat16.c
+++ oslab/lab4/lab4-code/simple_fat16.c
@@ -316,7 +316,7 @@ int find_root(FAT16 *fat16_ins, DIR_ENTRY *Dir, const char *path)
       Name_Buffer[j] = buffer[Start_Read+j];
     }
 
-    if (strcmp(Name_Buffer, paths[0])){
+    if (strcmp(Name_Buffer, paths[0])==0){
       if (pathDepth==1){
         for (j=0;j<11;++j){
           Dir->DIR_Name[j] = Name_Buffer[j];
